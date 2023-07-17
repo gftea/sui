0de455f775f1f5ac9e8abdca8a5e7c1b88102a52
sadhansood <sadhan@mystenlabs.com>
2023-07-17 14:30:38 -0700

    Improve db checkpoint handling

--- crates/sui-node/src/lib.rs
+++ crates/sui-node/src/lib.rs
@@ -154,7 +154,7 @@ pub struct SuiNode {
     /// Broadcast channel to notify state-sync for new validator peers.
     trusted_peer_change_tx: watch::Sender<TrustedPeerChangeEvent>,
 
-    _db_checkpoint_handle: Option<oneshot::Sender<()>>,
+    _db_checkpoint_handle: Option<tokio::sync::broadcast::Sender<()>>,
 
     #[cfg(msim)]
     sim_state: SimState,
@@ -672,7 +672,10 @@ impl SuiNode {
         config: &NodeConfig,
         prometheus_registry: &Registry,
         state_snapshot_enabled: bool,
-    ) -> Result<(DBCheckpointConfig, Option<oneshot::Sender<()>>)> {
+    ) -> Result<(
+        DBCheckpointConfig,
+        Option<tokio::sync::broadcast::Sender<()>>,
+    )> {
         let db_checkpoint_config = if config.db_checkpoint_config.checkpoint_path.is_none() {
             DBCheckpointConfig {
                 checkpoint_path: Some(config.db_checkpoint_path()),
@@ -700,7 +703,10 @@ impl SuiNode {
                     prometheus_registry,
                     state_snapshot_enabled,
                 )?;
-                Ok((db_checkpoint_config, Some(handler.start())))
+                Ok((
+                    db_checkpoint_config,
+                    Some(DBCheckpointHandler::start(handler)),
+                ))
             }
             None => Ok((db_checkpoint_config, None)),
         }
